cbeb9d8c5c946ab4f47422663b721f203c0b5e5a
Jen Fong-Adwent <jen@ednapiranha.com>
2012-07-07 15:45:56 -0400

    updated auth code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(function() {
   var loginForm = $('form#login-form');
 
--- test/test.authenticate.js
+++ test/test.authenticate.js
@@ -11,26 +11,30 @@ var qs = { assertion: '1a2b3c', audience: siteUrl };
 
 describe('login', function() {
   describe('POST /verify', function() {
-    it('logs the user in when they have good credentials', function() {
-      var scope = nock(authUrl).post('', qs).reply(200, { status: 'okay', email: 'test@test.org' });
+    it('logs the user in when they have valid credentials', function(done) {
+      var scope = nock(authUrl).post('/verify', qs).reply(200, { status: 'okay', email: 'bela@test.org' });
 
       var params = {
         body: { bid_assertion: qs.assertion }
       };
 
-      var authResp = auth.verify(params, nconf, function(error, email) { });
-      authResp.should.equal(true);
+      auth.verify(params, nconf, function(error, email) {
+        should.not.exist(error);
+        done();
+      });
     });
 
-    it('does not log the user in if they have bad credentials', function() {
+    it('does not log the user in if they have bad credentials', function(done) {
       var scope = nock(authUrl).post('', qs).reply(500, { status: 'invalid' });
 
       var params = {
         body: { }
       };
 
-      var authResp = auth.verify(params, nconf, function(error, email) { });
-      authResp.should.equal(false);
+      auth.verify(params, nconf, function(error, email) {
+        should.exist(error);
+        done();
+      });
     });
   });
 });
