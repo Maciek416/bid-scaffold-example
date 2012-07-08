31a453107c80f01a08664b4ae9646e4f065ae940
Jen Fong-Adwent <jen@ednapiranha.com>
2012-07-08 10:58:41 -0400

    fixed auth test

--- test/test.authenticate.js
+++ test/test.authenticate.js
@@ -1,38 +1,58 @@
+'use strict';
+
+var assert = require('should');
 var auth = require('../lib/authenticate');
-var should = require('should');
 var nock = require('nock');
+var should = require('should');
 var nconf = require('nconf');
+var querystring = require('querystring');
 
 nconf.argv().env().file({ file: 'test/local-test.json' });
 
-var authUrl = nconf.get('domain') + '/verify';
+var authUrl = nconf.get('authUrl');
 var siteUrl = nconf.get('domain') + ':' + nconf.get('port');
 var qs = { assertion: '1a2b3c', audience: siteUrl };
+var qsString = querystring.stringify(qs);
 
 describe('login', function() {
   describe('POST /verify', function() {
-    it('logs the user in when they have valid credentials', function(done) {
-      var scope = nock(authUrl).post('/verify', qs).reply(200, { status: 'okay', email: 'bela@test.org' });
-
-      var params = {
-        body: { bid_assertion: qs.assertion }
+    it('logs a user in if he/she has good credentials', function(done) {
+      var response = {
+        status: 'okay',
+        email: 'test@test.org'
       };
 
-      auth.verify(params, nconf, function(error, email) {
+      var scope = nock(authUrl).post('/verify', qsString).reply(200, response);
+
+      var params = {
+        body: {
+          bid_assertion: qs.assertion
+        }
+      };
+
+      var authResp = auth.verify(params, nconf, function(error, email) {
         should.not.exist(error);
+        email.should.equal(response.email);
         done();
       });
     });
 
-    it('does not log the user in if they have bad credentials', function(done) {
-      var scope = nock(authUrl).post('', qs).reply(500, { status: 'invalid' });
-
-      var params = {
-        body: { }
+    it('does not log a user in if he/she has bad credentials', function(done) {
+      var response = {
+        status: 'invalid'
       };
 
-      auth.verify(params, nconf, function(error, email) {
+      var scope = nock(authUrl).post('/verify', qsString).reply(500, response);
+
+      var params = {
+        body: {
+          bid_assertion: qs.assertion
+        }
+      };
+
+      var authResp = auth.verify(params, nconf, function(error, email) {
         should.exist(error);
+        error.status.should.equal(response.status);
         done();
       });
     });
