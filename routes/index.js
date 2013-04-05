d351919e26872f4a329f0088d1d26ad2ed52ee24
Jen Fong-Adwent <jen@ednapiranha.com>
2013-04-05 10:37:35 -0400

    removed unnecessary context; fixed url

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@
 
 module.exports = function(app, isLoggedIn) {
   app.get('/', function(req, res) {
-    res.render('index', {
-      pageType: 'index',
-      session: req.session
-    });
+    res.render('index');
   });
 };
